b4fe03c4a073c54cf84658abb798d6c1cf324e3f
Shawn Ballay <48398020+sballay8734@users.noreply.github.com>
2024-06-18 09:57:50 -0400

    chore: Start work on component refactor

--- src/components/ByArtistAndTitle.tsx
+++ src/components/ByArtistAndTitle.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Song } from "../data/mockSongdata";
 import ArtistFilter from "./ArtistFilter";
 import SongFilter from "./SongFilter";
 
@@ -6,14 +8,30 @@ interface Props {
 }
 
 export default function ByArtistAndTitle({ show }: Props): React.JSX.Element {
+  const [artistQuery, setArtistQuery] = useState<string>("");
+  const [selectedArtist, setSelectedArtist] = useState<string>("");
+
+  const [songTitleQuery, setSongTitleQuery] = useState<string>("");
+  const [selectedSong, setSelectedSong] = useState<Song>();
+
   return (
     <div
       className={`relative flex flex-col gap-2 ${
         show ? "translate-x-[100px] opacity-0" : "translate-x-0 opacity-100"
       } transition-all duration-200`}
     >
-      <ArtistFilter />
-      <SongFilter />
+      <ArtistFilter
+        artistQuery={artistQuery}
+        setArtistQuery={setArtistQuery}
+        selectedArtist={selectedArtist}
+        setSelecteArtist={setSelectedArtist}
+      />
+      <SongFilter
+        songTitleQuery={songTitleQuery}
+        setSongTitleQuery={setSongTitleQuery}
+        selectedSong={selectedSong}
+        setSelectedSong={setSelectedSong}
+      />
     </div>
   );
 }
