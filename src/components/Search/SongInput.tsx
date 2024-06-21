fd06808569754bb82c7dbfc145239affd71dcbb4
Shawn Ballay <48398020+sballay8734@users.noreply.github.com>
2024-06-21 12:24:29 -0400

    feat(search): complete logic to trigger song fetching when the selected artist changes

--- src/components/Search/SongInput.tsx
+++ src/components/Search/SongInput.tsx
@@ -1,71 +1,67 @@
-// import { useEffect, useState } from "react";
-// import { MdTitle } from "react-icons/md";
-// import { useAppSelector } from "../../hooks/hooks";
-// import { RootState } from "../../store/store";
+import { useEffect, useState } from "react";
+import { MdTitle } from "react-icons/md";
+import { useAppSelector } from "../../hooks/hooks";
+import { RootState } from "../../store/store";
 
-// export default function SongInput(): React.JSX.Element {
-//   const selectedArtist = useAppSelector(
-//     (state: RootState) => state.songSearch.selectedArtist,
-//   );
-//   // mTODO: Need to add type for array here vvv
-//   const [artistSongs, setArtistSongs] = useState([]);
+export default function SongInput(): React.JSX.Element {
+  const selectedArtist = useAppSelector(
+    (state: RootState) => state.songSearch.selectedArtist,
+  );
+  // mTODO: Need to add type for array here vvv
+  const [artistSongs, setArtistSongs] = useState([]);
 
-//   // Everytime artist selection changes, clear song list and fetch new songs
-//   useEffect(() => {
-//     console.log("CHANGED!");
-//   }, [selectedArtist]);
+  // Everytime artist selection changes, clear song list and fetch new songs
+  useEffect(() => {
+    if (selectedArtist) {
+      fetchSongs(selectedArtist?.artistId);
+    }
+  }, [selectedArtist]);
 
-//   console.log(selectedArtist);
+  console.log(selectedArtist);
 
-//   // fetchSongs by selectedArtist
-//   async function fetchSongs(query: string) {
-//     const params = new URLSearchParams({
-//       // access_token: import.meta.env.VITE_GENIUS_ACCESS_TOKEN,
-//       q: query,
-//       per_page: "5", // Adjust as needed
-//       page: "1", // Adjust if you want to implement pagination
-//     });
+  // fetchSongs by selectedArtist
+  async function fetchSongs(artistId: number) {
+    const getAllSongsQuery = `/proxy/api/artists/${artistId.toString()}/songs`;
 
-//     const artistQuery = `/proxy/api/search/artist?${params}`;
+    try {
+      const res = await fetch(getAllSongsQuery);
 
-//     try {
-//       const res = await fetch(artistQuery, { signal });
+      const data = await res.json();
+      console.log(data);
+      return;
 
-//       const data = await res.json();
-//       console.log(data);
+      const songsArray = data.response.sections[0].hits.map((item: Hit) => {
+        return {
+          artistName: item.result.name,
+          artistSlug: item.result.slug,
+          artistId: item.result.id,
+        };
+      });
 
-//       const artistsArray = data.response.sections[0].hits.map((item: Hit) => {
-//         return {
-//           artistName: item.result.name,
-//           artistSlug: item.result.slug,
-//           artistId: item.result.id,
-//         };
-//       });
+      setArtists(artistsArray);
+    } catch (error) {
+      if (error instanceof DOMException) {
+        // console.log("Intentional DOMException Error");
+        return;
+      }
+      console.log(error);
+    }
+  }
 
-//       setArtists(artistsArray);
-//     } catch (error) {
-//       if (error instanceof DOMException) {
-//         // console.log("Intentional DOMException Error");
-//         return;
-//       }
-//       console.log(error);
-//     }
-//   }
-
-//   return (
-//     <label className="flex items-center gap-2 bg-base-300 py-4 px-4 rounded-sm group border-[1px] border-transparent hover:border-primary hover:bg-primary/5 transition-colors duration-200">
-//       <input
-//         type="text"
-//         className="grow bg-transparent outline-0 border-0 placeholder:text-neutral-content/50"
-//         placeholder="Song Title"
-//         maxLength={100}
-//       />
-//       <MdTitle
-//         size={18}
-//         className="text-neutral-content/50 group-hover:text-primary transition-colors duration-200"
-//       />
-//     </label>
-//   );
-// }
+  return (
+    <label className="flex items-center gap-2 bg-base-300 py-4 px-4 rounded-sm group border-[1px] border-transparent hover:border-primary hover:bg-primary/5 transition-colors duration-200">
+      <input
+        type="text"
+        className="grow bg-transparent outline-0 border-0 placeholder:text-neutral-content/50"
+        placeholder="Song Title"
+        maxLength={100}
+      />
+      <MdTitle
+        size={18}
+        className="text-neutral-content/50 group-hover:text-primary transition-colors duration-200"
+      />
+    </label>
+  );
+}
 
 // !TODO: This will be interesting because Song Title input needs to be a dropdown AND a search bar
